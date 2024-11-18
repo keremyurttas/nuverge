bba1eb94b53f4579a3643f406d7b4c20972822ed
yusikerem <kerem.yky@gmail.com>
2024-11-18 23:19:21 +0300

    add connections page

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -9,3 +9,14 @@ export const WorkflowformSchema = z.object({
   name: z.string().min(1, "Required"),
   description: z.string().min(1, "Required"),
 });
+export type ConnectionTypes = 'Google Drive' | 'Notion' | 'Slack' | 'Discord'
+
+export type Connection = {
+  title: ConnectionTypes
+  description: string
+  image: string
+  connectionKey: any
+  accessTokenKey?: string
+  alwaysTrue?: boolean
+  slackSpecial?: boolean
+}
